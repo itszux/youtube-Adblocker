101683b859df8ab3aa30116a1eb1524cf0c0e279
itszux <99715555+itszux@users.noreply.github.com>
2023-10-20 22:14:43 +0300

    Update youtube adblocker.js

--- youtube adblocker.js
+++ youtube adblocker.js
@@ -1,15 +1,15 @@
 function getVideo() {
-	document.documentElement.setAttribute('normal_video', true)
-	try{document.getElementById('embededVideo').remove()}catch(err){}
-	const iframe = document.createElement('iframe');
-	iframe.id = 'embededVideo'
-	iframe.style.width = '-webkit-fill-available';
-	iframe.style.height = '-webkit-fill-available'; 
-	iframe.src = location.href.replace('watch?v=', 'embed/');
-	waitForElm('#player').then((elm) => {
-		const container = document.querySelectorAll('#container.ytd-player')[0];
-		setTimeout(function() {container.appendChild(iframe)}, 2000)//timeout is added because for some reasone the iframe is being removed after its added immediately
-	});
+    document.documentElement.setAttribute('normal_video', true)
+    try{document.getElementById('embededVideo').remove()}catch(err){}
+    const iframe = document.createElement('iframe');
+    iframe.id = 'embededVideo'
+    iframe.style.width = '-webkit-fill-available';
+    iframe.style.height = '-webkit-fill-available'; 
+    iframe.src = location.href.replace('watch?v=', 'embed/');
+    waitForElm('#player').then((elm) => {
+        const container = document.querySelectorAll('#container.ytd-player')[0];
+        setTimeout(function() {container.appendChild(iframe)}, 2000)//timeout is added because for some reasone the iframe is being removed after its added immediately
+    });
 }
 
 function waitForElm(selector) {
@@ -32,14 +32,15 @@ function waitForElm(selector) {
 
 document.documentElement.setAttribute('normal_video', false)
 waitForElm('#container').then((elm) => {
-	if (location.href.includes('watch')) {getVideo()}
+    if (location.href.includes('watch')) {getVideo()}
 });
 
 let currentPage = location.href;
 setInterval(function(){
     if (currentPage != location.href) {
         currentPage = location.href;
+        try{document.getElementById('embededVideo').remove()}catch(err){}
         document.documentElement.setAttribute('normal_video', false)
-		if (location.href.includes('watch')) {getVideo()}
+        if (location.href.includes('watch')) {getVideo()}
     }
 }, 500);
